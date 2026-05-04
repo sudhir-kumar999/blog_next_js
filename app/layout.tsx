893d34bb334d978d76bfb8490f2d4ad27c4a4438
sudhir-kumar999 <sk2001may@gmail.com>
2026-05-04 21:32:16 +0530

    google analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 import { Analytics } from "@vercel/analytics/next";
 
 import Footer from "@/components/Footer";
@@ -39,15 +40,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="hi">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-W2MN41PJ83"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-W2MN41PJ83');
+          `}
+        </Script>
+
         <AuthProvider>
           <Navbar />
           <main>{children}</main>
-          <Footer/>
-          <Analytics/>
+          <Footer />
+          <Analytics />
         </AuthProvider>
       </body>
     </html>
